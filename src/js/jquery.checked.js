6474efd5d4169d659a652e9d2985922645510965
Denis Seleznev <hcodes@yandex.ru>
2016-05-10 01:25:40 +0300

    Добавил для чекбоксов неопределенное состояние

--- src/js/jquery.checked.js
+++ src/js/jquery.checked.js
@@ -1,10 +1,16 @@
 var $ = require('jquery');
 
 $.fn.checked = function(checked) {
-    if(typeof checked === 'undefined') {
-        return this[0].checked;
-    } else {
-        this[0].checked = checked;
+    var el = this[0];
+    if(arguments.length) {
+        if (typeof checked === 'undefined') {
+            el.indeterminate = true;
+        } else {
+            el.indeterminate = false;
+            el.checked = checked;
+        }
         return this;
+    } else {
+        return el.checked;
     }
 };
