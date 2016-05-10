2ecf24de03a8e936379ac27988083c41164b1804
Denis Seleznev <hcodes@yandex.ru>
2016-05-10 22:26:27 +0300

    Доработки для diff-режима

--- src/js/app.js
+++ src/js/app.js
@@ -111,20 +111,34 @@ var App = {
         resHTML.is(':visible') && resHTML.html(entityHighlight(result));
         resDiff.is(':visible') && resDiff.html(this.diff(value, result));
     },
+    getDiffTitle: function(sym) {
+        var title = '';
+        if (sym === '\u00A0') {
+            title = 'NO-BREAK SPACE';
+        } else if (sym === '\u202F') {
+            title = 'NARROW NO-BREAK SPACE';
+        } else if (sym === '\u2011') {
+            title = 'NON-BREAKING HYPHEN';
+        }
+
+        return title;
+    },
     diff: function(o, n) {
         var diff = JsDiff.diffChars(o, n),
             html = '';
 
         diff.forEach(function(part){
-            var val = str.escapeHTML(part.value);
+            var val = str.escapeHTML(part.value),
+                title = this.getDiffTitle(part.value);
+
             if(part.added) {
-                html += '<ins class="diff">' + val + '</ins>';
+                html += '<ins class="diff" title="' + title + '">' + val + '</ins>';
             } else if(part.removed) {
-                html += '<del class="diff">' + val + '</del>';
+                html += '<del class="diff" title="' + title + '">' + val + '</del>';
             } else {
                 html += val;
             }
-        });
+        }, this);
 
         return html;
     },
