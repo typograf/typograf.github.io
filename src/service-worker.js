e2b1faa9768e5cf4e2da0f02aed20293736529b7
Denis Seleznev <hcodes@yandex.ru>
2020-10-12 15:28:18 +0300

    :cyclone:

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('./service-worker.js')
+        .register('./build/service-worker.js')
         .then(() => {
             console.log('Service Worker Registered');
         });
