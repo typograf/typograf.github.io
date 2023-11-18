0d746cd326dc8718556ef28da822af112ea9776e
Denis Seleznev <hcodes@users.noreply.github.com>
2023-11-18 17:35:39 +0300

    Add snowflakes

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,15 +1,34 @@
 import './header.css';
 
+import SnowFlakes from 'magic-snowflakes';
+import 'magic-snowflakes/dist/index.css';
+
 interface HeaderParams {
     onClick: () => void;
 }
 
 export class Header {
     private dom = document.querySelector('.header') as HTMLDivElement;
+    private snow: HTMLDivElement;
 
     constructor(params: HeaderParams) {
         this.dom.addEventListener('click', () => {
             params.onClick();
         });
+
+        this.snow = document.createElement('div');
+        this.snow.className = 'header__snow';
+
+        this.dom.appendChild(this.snow);
+
+        const snowflakes = new SnowFlakes({
+            container: this.snow,
+            color: '#ffffff',
+            count: 20,
+            speed: 0.3,
+            height: 300,
+        });
+
+        snowflakes.start();
     }
 }
