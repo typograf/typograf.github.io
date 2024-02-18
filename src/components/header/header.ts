356b5303202be25564c5b5d26ae18b57aa83f82d
Denis Seleznev <hcodes@users.noreply.github.com>
2024-02-18 11:40:15 +0300

    Remove snowflakes

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,8 +1,5 @@
 import './header.css';
 
-import SnowFlakes from 'magic-snowflakes';
-import 'magic-snowflakes/dist/index.css';
-
 interface HeaderParams {
     onClick: () => void;
 }
@@ -20,15 +17,5 @@ export class Header {
         this.snow.className = 'header__snow';
 
         this.dom.appendChild(this.snow);
-
-        const snowflakes = new SnowFlakes({
-            container: this.snow,
-            color: '#ffffff',
-            count: 20,
-            speed: 0.3,
-            height: 300,
-        });
-
-        snowflakes.start();
     }
 }
