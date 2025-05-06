ac393932f1f074f4a9054a12950d7af540f9bbb7
Denis Seleznev <hcodes@users.noreply.github.com>
2025-05-06 21:58:40 +0300

    🌀

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import css from 'rollup-plugin-postcss';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-
 import commonjs from '@rollup/plugin-commonjs';
 
 export default [
