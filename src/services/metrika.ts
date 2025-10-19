1a93ab3adf5b2a109de1d5a9a905f426f24fce1e
Denis Seleznev <hcodes@users.noreply.github.com>
2025-10-19 14:24:06 +0300

    track JS errors

--- src/services/metrika.ts
+++ src/services/metrika.ts
@@ -1,4 +1,4 @@
-import { LyamParams, hit, notBounce, reachGoal } from 'lyam';
+import { LyamParams, hit, notBounce, reachGoal, params } from 'lyam';
 
 const COUNTER_ID = '28700106';
 const NOT_BOUNCE_TIMEOUT = 15000; // 15 сек.
@@ -9,6 +9,8 @@ export function startMetrika() {
     setTimeout(() => {
         notBounce(COUNTER_ID);
     }, NOT_BOUNCE_TIMEOUT);
+
+    trackJs();
 
     console.info('metrika: hit');
 }
@@ -31,4 +33,39 @@ export function metrikaReachGoal(name: MetrikaGoal, params?: LyamParams) {
     reachGoal(COUNTER_ID, name, params);
 
     console.info('metrika: reachGoal', name, params);
+}
+
+export function metrikaParams(lyamParams: LyamParams) {
+    params(COUNTER_ID, lyamParams);
+
+    console.info('metrika: params', lyamParams);
+}
+
+function trackJs() {
+    window.addEventListener('error', event => {
+        if (!event) {
+            return;
+        }
+
+        const message = event.message || event.error?.message || 'no_message';
+        const stack = event.error?.stack || (event.filename + ':' + event.lineno + ':' + event.colno) || 'no_stack';
+        const path: string[] = [
+            'JS errors',
+            message,
+            stack,
+        ];
+
+        const data: { [key: string]: unknown; } = {};
+        let pointer = data;
+        let i: number;
+
+        for (i = 0; i < path.length - 1; i++) {
+            const item = path[i];
+            pointer = pointer[item] = {};
+        }
+
+        pointer[path[i]] = 1;
+
+        metrikaParams(data);
+    });
 }
