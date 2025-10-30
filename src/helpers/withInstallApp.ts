766bb273779182f84db4f13f2b5f9bfb30cb0196
Denis Seleznev <hcodes@users.noreply.github.com>
2025-10-30 09:50:14 +0300

    Fix ESLint warning

--- src/helpers/withInstallApp.ts
+++ src/helpers/withInstallApp.ts
@@ -1,5 +1,5 @@
 export function withInstallApp() {
-    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let deferredPrompt;
     window.addEventListener('beforeinstallprompt', e => {
         deferredPrompt = e;
